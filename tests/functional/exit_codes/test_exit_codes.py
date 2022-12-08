c48fe0201676552b1700f37487492be58a0883de
Michelle Ark <michelle.ark@dbtlabs.com>
2022-12-07 20:32:25 -0500

    split-group strategy for test-schema job

--- tests/functional/exit_codes/test_exit_codes.py
+++ tests/functional/exit_codes/test_exit_codes.py
@@ -50,7 +50,6 @@ class TestExitCodes(BaseConfigProject):
 
     def test_snapshot_pass(self, project):
         run_dbt(["run", "--model", "good"])
-
         results = run_dbt(['snapshot'])
         assert len(results) == 1
         check_table_does_exist(project.adapter, 'good_snapshot')
