0eed6fe8ae427a7c56e77b2071baa273633ae6cd
Jeremy Cohen <jeremy@dbtlabs.com>
2023-01-14 00:03:14 +0100

    Adapt for dbt-snowflake

--- tests/adapter/dbt/tests/adapter/caching/test_caching.py
+++ tests/adapter/dbt/tests/adapter/caching/test_caching.py
@@ -37,17 +37,27 @@ class BaseCachingTest:
         if not run_args:
             run_args = ['run']
         run_dbt(run_args)
+
+        # the cache was empty at the start of the run.
+        # the model materialization returned an unquoted relation and added to the cache.
         adapter = project.adapter
         assert len(adapter.cache.relations) == 1
         relation = next(iter(adapter.cache.relations.values()))
         assert relation.inner.schema == project.test_schema
         assert relation.schema == project.test_schema.lower()
 
+        # on the second run, dbt will find a relation in the database during cache population.
+        # this relation will be quoted, because list_relations_without_caching (by default) uses
+        # quote_policy = {"database": True, "schema": True, "identifier": True}
+        # when adding relations to the cache.
         run_dbt(run_args)
         adapter = project.adapter
         assert len(adapter.cache.relations) == 1
         second_relation = next(iter(adapter.cache.relations.values()))
-        assert relation.inner == second_relation.inner
+
+        # perform a case-insensitive + quote-insensitive comparison
+        for key in ["database", "schema", "identifier"]:
+            assert relation.inner[key].lower() == second_relation.inner[key].lower()
 
     def test_cache(self, project):
         self.run_and_inspect_cache(project)
